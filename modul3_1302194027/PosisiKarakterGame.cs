b8b8493afe6e9ae54864494adc6c9b8b39fe06c0
Muhammad Fauzi Dwikurnia <muhfauzidk@gmail.com>
2022-03-12 13:50:11 +0700

    menambahkan class PosisiKarakterGame

---
+++ modul3_1302194027/PosisiKarakterGame.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class PosisiKarakterGame
+{
+    enum State { Terbang, Jongkok, Berdiri, EXIT };
+    public static void Main()
+    {
+        State state = State.Berdiri;
+        string[] screenName = { "Terbang", "Jongkok", "Berdiri" };
+        string[] outputmsg = { "posisi landing", "", "posisi take off" };
+        while (state != State.EXIT)
+        {
+            Console.WriteLine("state: " + screenName[(int)state]);
+            Console.Write("Input Command: ");
+            string command = Console.ReadLine();
+            Console.WriteLine("Output: " + outputmsg[(int)state]);
+            Console.WriteLine("");
+            switch (state)
+            {
+                case State.Terbang:
+                    if (command == "Jongkok")
+                        state = State.Jongkok;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.Terbang;
+                    break;
+                case State.Jongkok:
+                    if (command == "Berdiri")
+                        state = State.Berdiri;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.Jongkok;
+                    break;
+                case State.Berdiri:
+                    if (command == "Terbang")
+                        state = State.Terbang;
+                    else if (command == "QUIT")
+                        state = State.EXIT;
+                    else
+                        state = State.Berdiri;
+                    break;
+            }
+        }
+        Console.WriteLine("EXIT SCREEN");
+    }
+}
--- modul3_1302194027/Program.cs
+++ modul3_1302194027/Program.cs
@@ -24,8 +24,6 @@ public class KodeBuah
             Console.WriteLine(getKodeBuah(i));
             i++;
         }
-
-
     }
 }
 
